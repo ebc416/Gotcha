b05d5966c0bb4e23b0419c80c849ac1572969e62
Khoa Le <khoa143@csu.fullerton.edu>
2020-03-02 10:30:15 -0800

    More updates

--- guiTest.py
+++ guiTest.py
@@ -7,7 +7,9 @@ from tkinter.filedialog import askopenfile
 
 root = tk.Tk()
 root.title("GOTCHA")
+root2 = tk.Toplevel()
 logo = tk.PhotoImage(file="Gotcha.png")
+logo2 = tk.PhotoImage(file="FBI.gif")
 #fileName2 = filedialog.askopenfilename()
 
 
@@ -15,7 +17,6 @@ top_frame = tk.Frame(root).pack()
 bottom_frame = tk.Frame(root).pack(side = "bottom")
 
 w1 = tk.Label(root, image=logo).pack(side="top",pady = 10)
-
 
 
 T = tk.Text(root, height=10, width=30,borderwidth = 2, relief="groove")
@@ -49,6 +50,22 @@ def open_file2():
 
 btn3 = tk.Button(root,bottom_frame,text = "Import File", fg = "red",command=open_file1).pack(side = "right")
 btn4 = tk.Button(root,bottom_frame,text = "Import File", fg = "green",command=open_file2).pack(side = "left")
+
+def alert_popup(title, message):
+    canvas = tk.Canvas(root2,width = 300, height = 200, bg = 'white')
+    canvas.pack(expand = YES, fill = BOTH)
+    gif1 = PhotoImage(file = '/Users/khoale/Documents/GitHub/Gotcha/FBI.gif')
+    canvas.create_image(50, 10, image = gif1, anchor = NW)
+    #popGif = tk.Label(root2, image=logo2).pack(side="top",pady = 10)
+    w = 400     # popup window width
+    h = 200     # popup window height
+    m = message
+    m += '\n'
+    w = Label(root2, text=m, width=69, height=5)
+    w.pack()
+    b = Button(root2, text="OK", command=root2.destroy, width=10)
+    b.pack()
+    mainloop()
 
 #widget_object = Progressbar(parent, **options)
 # Progress bar widget
@@ -103,6 +120,8 @@ def bar():
     DisplayM = Message(root, text = pDisplay)
     DisplayM.config(width=50)
     DisplayM.pack()
+    if pDisplay == 100:
+        alert_popup("Gottem!","100%  Plagiarism")
 
 progress.pack(pady = 1,side = "top")
 # This button will initialize
