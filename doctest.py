8b1402d16d86017952c0d016c6c47c3c648046f9
Khoa Le <khoa143@csu.fullerton.edu>
2020-04-09 11:05:04 -0700

    Update

---
+++ doctest.py
@@ -0,0 +1,3 @@
+import docx2txt
+my_text = docx2txt.process("Testing123.docx")
+print(my_text)
--- guiTest.py
+++ guiTest.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 import time
 import base64
-#from tkinter import filedialog
+import docx2txt
 from tkinter.ttk import *
 from tkinter import *
-from tkinter.filedialog import askopenfile
+from tkinter import filedialog
 from urllib.request import urlopen
 from PIL import Image
 import requests
 from functions import pCheck
-
+from functions import efrainspChecker
 
 
 root = tk.Tk()
@@ -31,6 +31,7 @@ logo2 = tk.PhotoImage(data = image_FBI)
 top_frame = tk.Frame(root).pack()
 bottom_frame = tk.Frame(root).pack(side = "bottom")
 
+
 w1 = tk.Label(root, image = logo).pack(side="top",pady = 10)
 
 
@@ -40,26 +41,41 @@ T.pack(side=tk.RIGHT,padx=10)
 T2 = tk.Text(root, height=10, width=30,borderwidth = 2, relief="groove")
 T2.pack(side=tk.LEFT, padx=10)
 
-#def UploadAction():filename = filedialog.askopenfilename()
+
 content = "tested"
+
 def open_file1():
-    file = askopenfile(mode ='r', filetypes =[('Text files', '*.txt')])
+    file = filedialog.askopenfilename(title="Select File", filetypes=(("Word files", "*.docx"), ("Text files", "*.txt")))
+
+
     if file is not None:
         global content
-        content = file.read()
-        #print(content)
+        if file[-3:] == "ocx":
+            content = docx2txt.process(file)
+        elif file[-3:] == "txt":
+            infile = open(file,'r')
+            content = infile.read()
+
+
     T.configure(state=NORMAL)
     T.delete(0.0, "end")
     T.insert(0.0,content)
     T.configure(state=DISABLED)
 
+
 content2 = "test"
 def open_file2():
-    file = askopenfile(mode ='r', filetypes =[('Text files', '*.txt')])
+    file = filedialog.askopenfilename(title="Select File", filetypes= ( ("Word files", "*.docx"), ("Text files", "*.txt") ) )
+
     if file is not None:
         global content2
-        content2 = file.read()
-        #print(content)
+        if file[-3:] == "ocx":
+            content2 = docx2txt.process(file)
+        elif file[-3:] == "txt":
+            infile = open(file,'r')
+            content2 = infile.read()
+
+
     T2.configure(state=NORMAL)
     T2.delete(0.0, "end")
     T2.insert(0.0,content2)
@@ -67,13 +83,13 @@ def open_file2():
 
 btn3 = tk.Button(root,bottom_frame,text = "Import File", fg = "red",command=open_file1).pack(side = "right")
 btn4 = tk.Button(root,bottom_frame,text = "Import File", fg = "green",command=open_file2).pack(side = "left")
-#/Users/khoale/Documents/GitHub/Gotcha
+
 def alert_popup(title, message):
     root2 = tk.Toplevel()
     canvas = tk.Canvas(root2,width = 925, height = 500, bg = 'white')
     canvas.pack(expand = YES, fill = BOTH)
     canvas.create_image(50, 50, image = logo2, anchor = NW)
-    #popGif = tk.Label(root2, image=logo2).pack(side="top",pady = 10)
+
     w = 400     # popup window width
     h = 200     # popup window height
     m = message
@@ -84,7 +100,6 @@ def alert_popup(title, message):
     b.pack()
     mainloop()
 
-#widget_object = Progressbar(parent, **options)
 # Progress bar widget
 pDisplay ='Plagiarism Percentage'
 DisplayM = Message(root, text = pDisplay)
@@ -98,27 +113,29 @@ progress = Progressbar(root, orient = HORIZONTAL,
 
 
 def bar():
-    S_value = pCheck(content,content2)
+    #S_value = pCheck(content,content2)
+    #changes into new algo
+    #S_value = pCheck(content,content2)
+    S_value = efrainspChecker(content,content2)
     import time
     progress['value'] = S_value
     pDisplay = progress['value']
-    DisplayM = Message(root, text = pDisplay)
+    DisplayM = Message(root, text = str(pDisplay)+"%")
     DisplayM.pack()
     root.update_idletasks()
     time.sleep(1)
 
     progress['value'] = S_value
     pDisplay = progress['value']
-    DisplayM.config(text = pDisplay)
+    DisplayM.config(text = str(pDisplay)+"%")
     DisplayM.config(width=50)
     DisplayM.pack()
     if pDisplay == 100:
         alert_popup("Gottem!","100%  Plagiarism")
 
 progress.pack(pady = 1,side = "top")
-# This button will initialize
-# the progress bar
-Button(root, text = 'Scan', command = bar).pack(pady = 10, side = "bottom")
+
+Button(root, text = 'Scan', command = bar).pack(pady = 10, side = "top")
 
 
 tk.mainloop()
