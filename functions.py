e17231ccfaec32e687bf5610b90a6acd8327756b
Khoa Le <khoa143@csu.fullerton.edu>
2020-04-11 13:53:09 -0700

    Update

--- functions.py
+++ functions.py
@@ -19,16 +19,17 @@ def efrainspChecker(file1_data,file2_data):
     return fuzz.partial_ratio(file1_data,file2_data)
 
 def read_report(file_one,file_two):
-    with open(file_one,'r') as file1:
-        with open(file_two,'r') as file2:
-            if file_one[-3:] == "ocx":
-                content = docx2txt.process(file_one)
-                content2 = docx2txt.process(file_two)
-                same = set(content).intersection(content2)
-            elif file_one[-3:] == "txt":
-                same = set(file1).intersection(file2)
+    if (file_one != None and file_two != None):
+        with open(file_one,'r') as file1:
+            with open(file_two,'r') as file2:
+                if file_one[-3:] == "ocx":
+                    content = docx2txt.process(file_one)
+                    content2 = docx2txt.process(file_two)
+                    same = set(content).intersection(content2)
+                elif file_one[-3:] == "txt":
+                    same = set(file1).intersection(file2)
 
-    same.discard('\n')
-    with open('report.txt','w') as file_out:
-        for line in same:
-            file_out.write(line)
+        same.discard('\n')
+        with open('report.txt','w') as file_out:
+            for line in same:
+                file_out.write(line)
