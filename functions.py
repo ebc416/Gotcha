057af091772031dd871f39bfea62886d468421eb
Khoa Le <khoa143@csu.fullerton.edu>
2020-04-14 10:56:20 -0700

    Update
    Slight improvement on overall look
    New stats feature

--- functions.py
+++ functions.py
@@ -2,14 +2,19 @@
 import re
 import difflib
 import docx2txt
+import math
+import string
+import sys
 from difflib import SequenceMatcher
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+translation_table = str.maketrans(string.punctuation + string.ascii_uppercase," " * len(string.punctuation) + string.ascii_lowercase)
 
 def pCheck(file1_data,file2_data):
     similarity_ratio = SequenceMatcher(None,file1_data,file2_data).ratio()
     return int(similarity_ratio * 100)
 
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
 
 def efrainspChecker(file1_data,file2_data):
     #for testing purposes
@@ -20,16 +25,122 @@ def efrainspChecker(file1_data,file2_data):
 
 def read_report(file_one,file_two):
     if (file_one != None and file_two != None):
-        with open(file_one,'r') as file1:
-            with open(file_two,'r') as file2:
-                if file_one[-3:] == "ocx":
-                    content = docx2txt.process(file_one)
-                    content2 = docx2txt.process(file_two)
-                    same = set(content).intersection(content2)
-                elif file_one[-3:] == "txt":
-                    same = set(file1).intersection(file2)
+        # with open(file_one,'r') as file1:
+        #     with open(file_two,'r') as file2:
+        if file_one[-3:] == "ocx":
+            content = docx2txt.process(file_one)
+            content2 = docx2txt.process(file_two)
+            file1 = set(line.strip() for line in open(content))
+            file2 = set(line.strip() for line in open(content2))
+            #same = set(content).intersection(content2)
+            #elif file_one[-3:] == 'txt'
+                #same = set(file1).intersection(file2)
+        else:
+            list1 = open(file_one).readlines()
+            list2 = open(file_two).readlines()
 
-        same.discard('\n')
-        with open('report.txt','w') as file_out:
-            for line in same:
-                file_out.write(line)
+        #with open('report.txt','w') as file_out:
+        file3 = open('report.txt','w')
+        for i in list1:
+            for j in list2:
+                if i == j:
+                    file3.write(i)
+                    # file3.write("FILE 2:", j)
+                    #file_out.write(line + "\n")
+        #same.discard('\n')
+            #with open('report.txt','w') as file_out:
+        #for line in same:
+            #file_out.write(line)
+
+def highlight(text,seq):
+    # get string to look for (if empty, no searching)
+    #s = "document has stayed the"
+    s = seq
+    if s:
+        # start from the beginning (and when we come to the end, stop)
+        idx = '1.0'
+        while 1:
+            # find next occurrence, exit loop if no more
+            idx = text.search(s, idx, nocase=1, stopindex='end')
+            if not idx: break
+            # index right after the end of the occurrence
+            lastidx = '%s+%dc' % (idx, len(s))
+            # tag the whole occurrence (start included, stop excluded)
+            text.tag_add('found', idx, lastidx)
+            # prepare to search for next occurrence
+            idx = lastidx
+        # use a red foreground for all the tagged occurrences
+        text.tag_config('found', foreground='red')
+
+def get_words_from_line_list(text):
+
+    text = text.translate(translation_table)
+    word_list = text.split()
+
+    return word_list
+
+
+def count_frequency(word_list):
+
+    D = {}
+
+    for new_word in word_list:
+
+        if new_word in D:
+            D[new_word] = D[new_word] + 1
+
+        else:
+            D[new_word] = 1
+
+    return D
+
+
+def word_frequencies_for_file(text_content):
+
+    line_list = text_content
+    word_list = get_words_from_line_list(line_list)
+    freq_mapping = count_frequency(word_list)
+
+
+    return freq_mapping
+
+def stats(tcontent):
+    printout = "Stats:\n"
+    line_list = tcontent
+    word_list = get_words_from_line_list(line_list)
+    freq_mapping = count_frequency(word_list)
+
+    strLine = str(len(line_list))
+    strWrd = str(len(word_list))
+    strFrq = str(len(freq_mapping))
+    printout += str(strLine + " lines.\n")
+    printout += str(strWrd+ " lines.\n")
+    printout += str(strFrq + " unique words.\n")
+
+    return printout
+
+
+def dotProduct(D1, D2):
+    Sum = 0.0
+
+    for key in D1:
+
+        if key in D2:
+            Sum += (D1[key] * D2[key])
+
+    return Sum
+
+def vector_angle(D1, D2):
+    numerator = dotProduct(D1, D2)
+    denominator = math.sqrt(dotProduct(D1, D1)*dotProduct(D2, D2))
+
+    return math.acos(numerator / denominator)
+
+
+def documentSimilarity(store1,store2):
+
+    sorted_word_list_1 = word_frequencies_for_file(store1)
+    sorted_word_list_2 = word_frequencies_for_file(store2)
+
+    distance = vector_angle(sorted_word_list_1, sorted_word_list_2)
+    print("Similarity Score: % 0.6f "% distance)
--- guiTest.py
+++ guiTest.py
@@ -11,14 +11,19 @@ import requests
 from functions import pCheck
 from functions import efrainspChecker
 from functions import read_report
+from functions import documentSimilarity
+from functions import stats
+from functions import highlight
 
 
 
 
 root = tk.Tk()
 root.title("GOTCHA")
+root.geometry("880x600")
+root.resizable(0,0)
 url = "https://i.imgur.com/HZCR1G2.png"
-url2 = "https://i.imgur.com/ElKpHkG.png"
+url2 = "https://i.imgur.com/yzNki6P.png"
 
 respond = requests.get(url2)
 img_byt = urlopen(url2).read()
@@ -35,15 +40,19 @@ top_frame = tk.Frame(root).pack()
 bottom_frame = tk.Frame(root).pack(side = "bottom")
 
 
-w1 = tk.Label(root, image = logo).pack(side="top",pady = 10)
+w1 = tk.Label(root, image = logo,relief = "raised").pack(side="top",pady = 10)
 
-
-T = tk.Text(root, height=10, width=30,borderwidth = 2, relief="groove")
+T = tk.Text(root,top_frame,height=15, width=38,borderwidth = 2, relief="groove")
 T.pack(side=tk.RIGHT,padx=10)
 
-T2 = tk.Text(root, height=10, width=30,borderwidth = 2, relief="groove")
+T2 = tk.Text(root,top_frame, height=15, width=38,borderwidth = 2, relief="groove")
 T2.pack(side=tk.LEFT, padx=10)
 
+T3 = tk.Text(root,bottom_frame,height=5,width=30,borderwidth = 2, relief="ridge")
+T3.pack(side=tk.BOTTOM,pady=5)
+
+T3.insert(1.0,"          Statistics:\n")
+T3.configure(state=DISABLED)
 
 content = None
 fileone = None
@@ -66,7 +75,6 @@ def open_file1():
     T.configure(state=NORMAL)
     T.delete(0.0, "end")
     T.insert(0.0,content)
-    T.configure(state=NORMAL)
 
 
 content2 = None
@@ -89,8 +97,8 @@ def open_file2():
     T2.insert(0.0,content2)
     T2.configure(state=NORMAL)
 
-btn3 = tk.Button(root,bottom_frame,text = "Import File", fg = "red",command=open_file1).pack(side = "right")
-btn4 = tk.Button(root,bottom_frame,text = "Import File", fg = "green",command=open_file2).pack(side = "left")
+btn3 = tk.Button(root,bottom_frame,text = "Import File",relief = "raised", fg = "red",command=open_file1).pack(side = "right")
+btn4 = tk.Button(root,bottom_frame,text = "Import File",relief = "raised", fg = "green",command=open_file2).pack(side = "left")
 
 def alert_popup(title, message):
     root2 = tk.Toplevel()
@@ -111,7 +119,7 @@ def alert_popup(title, message):
 # Progress bar widget
 ptDisplay ='Plagiarism Percentage'
 DisplayM = Message(root, text = ptDisplay)
-DisplayM.config(bg='lightgreen',width=200)
+DisplayM.config(bg='lightgreen',width=200,relief="raised")
 DisplayM.pack(side = "top")
 
 progress = Progressbar(root, orient = HORIZONTAL,
@@ -123,11 +131,18 @@ scoreTracker = StringVar()
 scoreTracker.set("0%")
 global DisplayMsg
 DisplayMsg = Label(root,textvariable = scoreTracker)
-#DisplayMsg.pack()
+
 
 def bar():
-
     read_report(fileone,filesec)
+      #open similarFile and read line by line removing \n to search in our two text files
+    with open("real_similarities.txt") as similarFile:
+        sequence = similarFile.readlines()
+        for line in sequence:
+            highlight(T,line.strip())
+            highlight(T2,line.strip())
+            #print(line.strip())
+    similarFile.close()
 
     if fileone != None:
         #global DisplayMsg
@@ -139,15 +154,18 @@ def bar():
         pDisplay = progress['value']
         scoreTracker.set(str(pDisplay)+"%")
         #DisplayMsg = Label(root, textvariable = scoreTracker)
-        DisplayMsg.pack()
+        DisplayMsg.pack(pady = 10,side = "top")
         root.update_idletasks()
 
-
-        #progress['value'] = 100
-        #pDisplay = progress['value']
-        #DisplayM.config(text = str(pDisplay)+"%")
-        #DisplayM.config(width=50)
-        #DisplayM.pack()
+        loadIn = stats(content)
+        T3.configure(state=NORMAL)
+        #T3.delete(0.0, "end")
+        T3.insert(2.0,"Right-Side File\n")
+        T3.insert(3.0,loadIn)
+        loadIn2 = stats(content2)
+        T3.insert(8.0,"\nLeft-Side File\n")
+        T3.insert(9.0,loadIn2)
+        T3.configure(state=DISABLED)
 
         if pDisplay == 100:
             alert_popup("Gottem!","100%  Plagiarism")
@@ -160,27 +178,23 @@ def Clear():
     content2 = None
     fileone = None
     filesec = None
-    global DisplayMsg
     T.delete("1.0","end")
     T.update()
     T2.delete("1.0","end")
     T2.update()
+    T3.configure(state = NORMAL)
+    T3.delete("2.0","end")
+    T3.update()
+    T3.configure(state = DISABLED)
     progress['value'] = 0
     pDisplay = progress['value']
-    #DisplayMsg.config(text = str(pDisplay
     if pDisplay == 0:
         scoreTracker.set("")
-    elif content != "none":
+    elif content != None:
         scoreTracker.set(str(pDisplay)+"%")
-    #DisplayMsg.config(textvariable = scoreTracker)
-    #DisplayMsg.pack()
     root.update_idletasks()
 
-
-
-
 Button(root, text = 'Scan', command = bar).pack(pady = 10, side = "bottom")
-Button(root, text = 'Clear', command = Clear).pack(pady = 10, side = "top")
-
+Button(root, text = 'Clear', command = Clear).pack(pady = 25,side = "bottom")
 
 tk.mainloop()
